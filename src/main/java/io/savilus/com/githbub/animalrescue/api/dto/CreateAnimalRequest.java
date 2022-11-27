bc5f6e4018b70316784abd6297113ca25cc13937
Jakub Łanoszka <jakublanoszka@gmail.com>
2022-11-27 10:52:35 +0100

    add-validation-delete-animal

--- src/main/java/io/savilus/com/githbub/animalrescue/api/dto/CreateAnimalRequest.java
+++ src/main/java/io/savilus/com/githbub/animalrescue/api/dto/CreateAnimalRequest.java
@@ -1,20 +1,24 @@
 package io.savilus.com.githbub.animalrescue.api.dto;
 
 import lombok.Value;
+import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 
 @Value
 public class CreateAnimalRequest {
+
     @NotNull(message = "Name cannot be null")
     @NotBlank(message = "Name cannot be empty")
+    @Length(message = "Name have to be between 2 or 30", min = 2, max = 30)
     String name;
 
     @NotNull(message = "Age cannot be null")
     @Positive(message = "Age must be positive")
+    @Max(message = "Max age is 200", value = 200)
     Integer age;
+
+
 
 
 }
--- src/main/java/io/savilus/com/githbub/animalrescue/infastructure/DefaultAnimalService.java
+++ src/main/java/io/savilus/com/githbub/animalrescue/infastructure/DefaultAnimalService.java
@@ -1,59 +1,64 @@
 package io.savilus.com.githbub.animalrescue.infastructure;
 
 import io.savilus.com.githbub.animalrescue.domain.*;
+import io.savilus.com.githbub.animalrescue.repositories.AnimalsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class DefaultAnimalService implements AnimalService {
 
-    private final Map<String, Animal> animals = new HashMap<>();
+    private final AnimalsRepository animalsRepository;
+
+    public DefaultAnimalService(AnimalsRepository animalsRepository) {
+        this.animalsRepository = animalsRepository;
+    }
 
 
     @Override
     public List<Animal> listOfAnimals(Integer limit) {
-        return animals.values().stream().limit(limit.longValue()).toList();
+        return animalsRepository.findAllAnimals(limit);
     }
 
     @Override
     public Animal singleAnimal(String id) {
-        return animals.get(id);
+        return animalsRepository.findAnimal(id);
     }
 
     @Override
     public Animal createAnimal(Specie specie, Integer age, String name) {
         // losowy generator, bardzo mała kolizyjność
         UUID uuid = UUID.randomUUID();
+        Animal animal;
 
         switch (specie) {
             case DOG -> {
-                Animal animal = new Dog(uuid.toString(),name, age);
-                animals.put(animal.getId(),animal);
-                return animal;
+                animal = new Dog(uuid.toString(), name, age);
             }
             case CAT -> {
-                Animal animal = new Cat(uuid.toString(),name, age);
-                animals.put(animal.getId(),animal);
-                return animal;
+                animal = new Cat(uuid.toString(), name, age);
             }
             case ELEPHANT -> {
-                Animal animal = new Elephant(uuid.toString(), name, age);
-                animals.put(animal.getId(),animal);
-                return animal;
+                animal = new Elephant(uuid.toString(), name, age);
             }
             default -> {
                 throw new IllegalStateException("Unsupported specie ");
             }
+
         }
+
+        animalsRepository.saveAnimal(animal);
+        return animal;
     }
 
     @Override
     public boolean deleteAnimal(String id) {
-        animals.remove(id);
+        animalsRepository.deleteAnimal(id);
+        log.info("Deleting animal by ID {}", id);
         return true;
     }
 }
