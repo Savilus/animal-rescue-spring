20caa5bc3da6f120dcd39bfa154951889717690c
Jakub Łanoszka <jakublanoszka@gmail.com>
2022-11-27 15:50:50 +0100

    ParameterizedTest

--- src/test/java/io/savilus/com/githbub/animalrescue/AnimalRescueApplicationTests.java
+++ src/test/java/io/savilus/com/githbub/animalrescue/AnimalRescueApplicationTests.java
@@ -1,14 +1,21 @@
 package io.savilus.com.githbub.animalrescue;
 
 import io.savilus.com.githbub.animalrescue.api.dto.AllAnimalsResponse;
+import io.savilus.com.githbub.animalrescue.api.dto.ErrorDto;
 import io.savilus.com.githbub.animalrescue.api.dto.SingleAnimalResponse;
 import io.savilus.com.githbub.animalrescue.domain.Dog;
 import io.savilus.com.githbub.animalrescue.domain.Specie;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -79,5 +86,40 @@ class AnimalRescueApplicationTests {
         assertThat(allDogsEntity.getBody().getSpecie()).isEqualTo(expectedSpecie);
 
     }
+    @ParameterizedTest
+    @MethodSource("badRequestArguments")
+    void shouldReturnBadRequestWhenAgeIsNegative(String name, Integer age, String message){
+        // given
+        String animalBody = """
+                {"name": "%s",
+                "age": %d
+                }
+                """;
+        String formattedAnimalBody = String.format(animalBody, name, age);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<String> stringHttpEntity = new HttpEntity<>(formattedAnimalBody, httpHeaders);
+        String postAnimalUrl = "/animals/dogs";
+        HttpStatus expectedStatus = HttpStatus.BAD_REQUEST;
+
+        //when
+        ResponseEntity<ErrorDto> stringResponseEntity =
+                restTemplate.postForEntity(postAnimalUrl, stringHttpEntity, ErrorDto.class);
+        assertThat(stringResponseEntity.getStatusCode()).isEqualTo(expectedStatus);
+        assertThat(stringResponseEntity.getBody().getMessage()).isEqualTo(message);
+    }
+
+    static Stream<Arguments> badRequestArguments(){
+      return Stream.of(
+              Arguments.of("Reksio", null , "age: Age cannot be null"),
+              Arguments.of("R", 10, "name: Name have to be between 2 or 30"),
+              Arguments.of("Burek", 2500, "age: Max age is 200"),
+              Arguments.of("    ", 10, "name: Name cannot be empty"),
+              Arguments.of("Nutka", null, "age: Age cannot be null"),
+              Arguments.of("Minutka", -3, "age: Age must be positive"));
+    }
+
+
+
 
 }
